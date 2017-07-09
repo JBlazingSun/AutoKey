adb132346ccf188d610d7f404c6549eea71c5977
blazings <328801898@qq.com>
2017-07-10 00:14:32 +0800

    keybd_event无效

--- AutoKeyCombin/AutoKeyCombin/Form1.cs
+++ AutoKeyCombin/AutoKeyCombin/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,14 +16,12 @@ using System.Windows.Forms;
 //6   迅捷  +D
 //7  隐身  +D
 //大写切换  电磁脉冲  +D
-//Shift    飓风  +D
+//左Shift    飓风  +D
 //ALT+3 精灵 +D
 //Alt+2  冰墙  +D
 //Alt+C  急速冷却 +D
 
-
-    //F10开
-    //F11关
+    //F11开关
 
 namespace AutoKeyCombin
 {
@@ -29,6 +29,12 @@ namespace AutoKeyCombin
     {
         private bool open = true;
         KeyboardHook kh;
+        public const int KEYDOWN = 0;
+        public const int KEYUP = 2;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
         public Form1()
         {
             InitializeComponent();
@@ -40,14 +46,71 @@ namespace AutoKeyCombin
 
         void kh_OnKeyDownEvent(object sender, KeyEventArgs e)
         {
-            if (e.KeyData == (Keys.F10) && open)
-            {
-
-            }
             if (e.KeyData == (Keys.F11))
             {
-                open = false;
+                open = !open;
             }
+            if (open)
+            {
+                if (e.KeyData == (Keys.F3))//天火
+                {
+                    keybd_event((byte) Keys.E, 0, KEYDOWN, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.E, 0, KEYUP, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.E, 0, KEYDOWN, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.E, 0, KEYUP, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.E, 0, KEYDOWN, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.E, 0, KEYUP, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.D, 0, KEYDOWN, 0);
+                    Thread.Sleep(100);
+                    keybd_event((byte)Keys.D, 0, KEYUP, 0);
+                    Thread.Sleep(100);
+                }
+                if (e.KeyData == (Keys.D4))//地狱火
+                {
+                    MessageBox.Show("地狱火");
+                }
+                if (e.KeyData == (Keys.D5))//冲击波
+                {
+                    MessageBox.Show("冲击波");
+                }
+                if (e.KeyData == (Keys.D6))//迅捷
+                {
+                    MessageBox.Show("迅捷");
+                }
+                if (e.KeyData == (Keys.D7))//隐身
+                {
+                    MessageBox.Show("隐身");
+                }
+                if (e.KeyData == (Keys.CapsLock))//电磁脉冲
+                {
+                    MessageBox.Show("电磁脉冲");
+                }
+                if (e.KeyData == (Keys.X| Keys.Alt))//飓风
+                {
+                    MessageBox.Show("飓风");
+                }
+                if (e.KeyData == (Keys.D3 | Keys.Alt))//精灵
+                {
+                    MessageBox.Show("精灵");
+                }
+
+                if (e.KeyData == (Keys.D2 | Keys.Alt))//冰墙
+                {
+                    MessageBox.Show("冰墙");
+                }
+
+                if (e.KeyData == (Keys.C | Keys.Alt))//急速冷却
+                {
+                    MessageBox.Show("急速冷却");
+                }
+            }
+
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
